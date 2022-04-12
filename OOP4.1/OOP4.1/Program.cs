29c45f010f477f738f1a8e9f84de07662479f9db
Romanovsk1 <93801993+Romanovsk1@users.noreply.github.com>
2022-04-12 21:49:11 +0400

    DZ4

---
+++ OOP4.1/OOP4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Building building = new();
+            building.Add(3, 6, 7);
+
+            Building building2 = new();
+            building2.Add(5, 7, 10, 76);
+
+            building.Print();
+            building2.Print();
+            building.Print();
+        }
+    }
+}
