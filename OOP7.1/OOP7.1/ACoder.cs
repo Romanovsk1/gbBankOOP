1d84babcdf04cdefd24b005b78af1bf2372668fd
Romanovsk1 <93801993+Romanovsk1@users.noreply.github.com>
2022-04-26 22:12:30 +0400

    DZ7

---
+++ OOP7.1/OOP7.1/ACoder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP7._1
+{
+    public class ACoder : ICoder
+    {
+        private readonly char[] _inputCharArray;
+
+        public ACoder(string inputString)
+        {
+            _inputCharArray = inputString.ToCharArray();
+        }
+
+        public string Decode()
+        {
+            for (int i = 0; i < _inputCharArray.Length; i++)
+            {
+                _inputCharArray[i] = (char)(_inputCharArray[i] - 1);
+            }
+
+            return string.Concat(_inputCharArray);
+        }
+
+        public string Encode()
+        {
+            for (int i = 0; i < _inputCharArray.Length; i++)
+            {
+                _inputCharArray[i] = (char)(_inputCharArray[i] + 1);
+            }
+
+            return string.Concat(_inputCharArray);
+        }
+    }
+}
---
+++ OOP7.1/OOP7.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var originalString = "welcome to the club buddy";
+
+            var ACoder = new ACoder(originalString);
+
+            Console.WriteLine($"{nameof(originalString)} =  {originalString}");
+            Console.WriteLine($"ACoder.Encode= {ACoder.Encode()}");
+            Console.WriteLine($"ACoder.Decode= {ACoder.Decode()}");
+
+
+            var BCoder = new BCoder(originalString);
+
+            Console.WriteLine($"{nameof(originalString)} =  {originalString}");
+            Console.WriteLine($"BCoder.Encode= {BCoder.Encode()}");
+            Console.WriteLine($"BCoder.Decode= {BCoder.Decode()}");
+
+            Console.ReadKey();
+        }
+    }
+}
